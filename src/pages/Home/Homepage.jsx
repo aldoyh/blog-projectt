f55f95648633318e29078701309ba55ed197c5a6
Hasan AlDoy <aldoyh@gmail.com>
2024-04-26 18:18:56 +0300

    Add Homepage component to the project

---
+++ src/pages/Home/Homepage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Homepage = () => {
+    return (
+        <div>
+            <h1>Welcome to My Blog</h1>
+            <p>This is the homepage of my blog project.</p>
+        </div>
+    );
+};
+
+export default Homepage;
